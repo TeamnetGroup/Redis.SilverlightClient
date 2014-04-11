20297537e2496303e570f1b748c0745ed4569513
Victor Grigoriu <vgrigoriu@gmail.com>
2014-04-11 18:14:23 +0300

    Separate parsers, add tests

--- Redis.SilverlightGap/Redis.MessageParsers/RedisParsersModule.cs
+++ Redis.SilverlightGap/Redis.MessageParsers/RedisParsersModule.cs
@@ -1,23 +1,13 @@
-﻿using PortableSprache;
-using System;
-using System.Linq;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Ink;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
+﻿using System.Linq;
+using PortableSprache;
 
 namespace Redis.SilverlightClient.Parsers
 {
-    internal static class RedisParsersModule
+    public static class RedisParsersModule
     {
         public static Parser<string> BulkStringParser =
                                         from dollar in Parse.Char('$')
-                                        from length in Parse.Number.Text().Select(int.Parse)
+                                        from length in Parse.Number.Select(int.Parse)
                                         from _ in Parse.String("\r\n")
                                         from content in Parse.AnyChar.Repeat(length)
                                         from __ in Parse.String("\r\n")
@@ -26,7 +16,7 @@ namespace Redis.SilverlightClient.Parsers
         public static Parser<string[]> ArrayOfBulkStringsParser =
                                         (
                                             from star in Parse.Char('*')
-                                            from arrayLength in Parse.Number.Text().Select(int.Parse)
+                                            from arrayLength in Parse.Number.Select(int.Parse)
                                             from _ in Parse.String("\r\n")
                                             from arrayElement in BulkStringParser.Repeat(arrayLength)
                                             select arrayElement
